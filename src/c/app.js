0d8e18b76a212ea545fb1f103371151ae138f51e
stognole <stognole@b-tu.de>
2017-05-14 11:30:49 +0200

    Überarbeitung Teil 3 (Testdaten vereinheitlicht)

--- src/c/app.js
+++ src/c/app.js
@@ -9,3 +9,54 @@ var pl = {
   v: { internationalOrganizations:{}, cities:{},countries:{}},
   c: { internationalOrganizations:{}, cities:{},countries:{}}
 };
+pl.c.app = {
+    initialize: function() {
+    },
+    createTestData: function() {
+        try {
+            City.instances["Berlin"] = new City({name:"Berlin"});
+            City.instances["Hamburg"] = new City({name:"Berlin"});
+            City.instances["Frankfurt"] = new City({name:"Berlin"});
+            City.instances["Paris"] = new City({name:"Berlin"});
+            City.instances["Moskau"] = new City({name:"Berlin"});
+            City.instances["Novosibirsk"] = new City({name:"Berlin"});
+            City.instances["London"] = new City({name:"Berlin"});
+            City.instances["Madrid"] = new City({name:"Berlin"});
+            City.saveAll();
+            Country.instances["Deutschland"] = new Country({
+                name: "Deutschland",
+                capital: "Berlin",
+                cities: {name: "Berlin"}});
+            Country.instances["Russland"] = new Country({
+                name: "Russland",
+                capital: "Moskau",
+                cities: {name: "Moskau"}});
+            Country.instances["Frankreich"] = new Country({
+                name: "Frankreich",
+                capital: "Paris",
+                cities: {name: "Paris"}});
+            Country.saveAll();
+            InternationalOrganizations.instances["ABC"] =
+                new InternationalOrganizations({
+                    acronym:"ABC",
+                    name:"A-B-C",
+                    members: {name: "Deutschland"}});
+            InternationalOrganizations.saveAll();
+        } catch (e) {
+            console.log( e.constructor.name + ": " + e.message);
+        }
+    },
+    clearData: function() {
+        try {
+            City.instances = {};
+            localStorage["cities"] = JSON.stringify({});
+            Country.instances = {};
+            localStorage["countries"] = JSON.stringify({});
+            InternationalOrganizations.instances = {};
+            localStorage["internationalOrganizations"] = JSON.stringify({});
+            console.log("All data cleared.");
+        } catch (e) {
+            console.log( e.constructor.name + ": " + e.message);
+        }
+    }
+};
