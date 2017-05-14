1e67db9e7e17243c13d1df402e037f3e61526cb4
stognole <stognole@b-tu.de>
2017-05-14 10:46:16 +0200

    Überarbeitung Teil 2

--- src/m/InternationalOrganizations.js
+++ src/m/InternationalOrganizations.js
@@ -10,39 +10,43 @@ function InternationalOrganizations(slots) {
   this.members = {};
   
   if (arguments.length > 0) {
-    this.setName( slots.name );
-    this.setAcronym( slots.acronym );
+    this.setName(slots.name);
+    this.setAcronym(slots.acronym);
     
     if (slots.members ||slots.membersIdRef) {
-      this.setMember( slots.members || slots.membersIdRef );
+      this.setMember(slots.members || slots.membersIdRef);
     }
   }
 }
 
 InternationalOrganizations.instances = {};
-/***************Check Name****************************/
+
+/**
+ * Check Name
+ **/
+
 InternationalOrganizations.checkName = function (name) {
   var constraintViolation;
   if (!name) {
     constraintViolation = new MandatoryValueConstraintViolation
-    ( "a name is required" );
+    ("a name is required");
   }
   else if (typeof(name) !== "string" || name.trim() === "") {
-    
     constraintViolation = new RangeConstraintViolation(
-        "The name must be a non-empty string!" );
-    
+        "The name must be a non-empty string!");
   }
   else {
-    constraintViolation = new NoConstraintViolation("alles gut");
+    constraintViolation = new NoConstraintViolation("Success");
   }
   return constraintViolation;
 };
 
+/**
+ * Set Name
+ **/
 
-/***************Set Name**************************************/
 InternationalOrganizations.prototype.setName = function (name) {
-  var validationResult = InternationalOrganizations.checkName( name );
+  var validationResult = InternationalOrganizations.checkName(name);
   
   if (validationResult instanceof NoConstraintViolation) {
     this.name = name;
@@ -52,7 +56,10 @@ InternationalOrganizations.prototype.setName = function (name) {
   }
 };
 
-/***************Check Acronym**************************************/
+/**
+ * Check Acronym
+ **/
+
 InternationalOrganizations.checkAcronym = function (acronym) {
   var constraintViolation;
   if (!acronym) {
@@ -61,7 +68,7 @@ InternationalOrganizations.checkAcronym = function (acronym) {
   
   else if (typeof(acronym) !== "string" || acronym.trim() === "") {
     constraintViolation = new RangeConstraintViolation(
-        "The name must be a non-empty string!" );
+        "The name must be a non-empty string!");
   }
   
   else {
@@ -69,18 +76,22 @@ InternationalOrganizations.checkAcronym = function (acronym) {
   }
   return constraintViolation;
 };
-/***************Check Acronym as ID********************************/
+
+/**
+ * Check Acronym as ID
+ **/
+
 InternationalOrganizations.checkAcronymAsId = function (acronym) {
-  var constraintViolation = InternationalOrganizations.checkAcronym( acronym );
+  var constraintViolation = InternationalOrganizations.checkAcronym(acronym);
   if (constraintViolation instanceof NoConstraintViolation) {
     console.log("acronym ist: "+ acronym);
-    if (!acronym  ) {
+    if (!acronym ) {
       constraintViolation = new MandatoryValueConstraintViolation
-      ( "a name is required" );
+      ("a name is required");
     }
     else if (InternationalOrganizations.instances[acronym]) {
       constraintViolation = new UniquenessConstraintViolation(
-          "There is already an organization record with this acronym!" );
+          "There is already an organization record with this acronym!");
     }
     
     else {
@@ -89,77 +100,85 @@ InternationalOrganizations.checkAcronymAsId = function (acronym) {
   }
   return constraintViolation;
 };
-/***************Check Acronym as ID Ref****************************/
+
+/**
+ * Check Acronym as ID Ref
+ **/
+
 InternationalOrganizations.checkAcronymAsIdRef = function (acronym) {
   
-  var constraintViolation = InternationalOrganizations.checkName( acronym );
+  var constraintViolation = InternationalOrganizations.checkName(acronym);
   if ((constraintViolation instanceof NoConstraintViolation)
       && acronym !== undefined) {
     if (!InternationalOrganizations.instances[acronym]) {
       constraintViolation = new ReferentialIntegrityConstraintViolation
-      ( "There is no city record with this name!" );
+      ("There is no city record with this name!");
     }
   }
   
   return constraintViolation;
 };
-/***************Set Acronym**************************************/
+
+/**
+ * Set Acronym
+ **/
+
 InternationalOrganizations.prototype.setAcronym = function (acronym) {
-  var validationResult = InternationalOrganizations.checkAcronymAsId( acronym );
-  
+  var validationResult = InternationalOrganizations.checkAcronymAsId(acronym);
   if (validationResult instanceof NoConstraintViolation) {
     this.acronym = acronym;
-    
   }
   else {
     throw validationResult;
   }
-  
-  
 };
-/***************Check Member**************************************/
+
+/**
+ * Check Member
+ **/
 InternationalOrganizations.checkMember = function (countryIdRef) {
   var constraintViolation = null;
   if (!countryIdRef) {
-    
-    constraintViolation = new NoConstraintViolation("alles gut");
+    constraintViolation = new NoConstraintViolation("Success");
   } else {
     // invoke foreign key constraint check
     constraintViolation =
-        Country.checkNameAsIdRef( countryIdRef );
+        Country.checkNameAsIdRef(countryIdRef);
   }
   return constraintViolation;
 };
 
-
-/***************Set Member**************************************/
+/**
+ * Set Member
+ **/
 InternationalOrganizations.prototype.setMember = function (members) {
-  var keys = [], i = 0;
+  var keys=[], i=0;
   this.members = {};
-  if (Array.isArray( members )) {  // array of IdRefs
+  if (Array.isArray(members)) {  // array of IdRefs
     for (i = 0; i < members.length; i += 1) {
-      this.addMember( members[i] );
+      this.addMember(members[i]);
     }
   } else {  // map of object refs
-    keys = Object.keys( members );
+    keys = Object.keys(members);
     for (i = 0; i < keys.length; i += 1) {
-      this.addMember( members[keys[i]] );
+      this.addMember(members[keys[i]]);
     }
   }
 };
 
+/**
+ * Add Member
+ **/
 
-/***************Add Member**************************************/
 InternationalOrganizations.prototype.addMember = function (member) {
   var constraintViolation = null,
       countryIdRef = 0, countryIdRefStr = "";
-  
-  if (typeof( member) !== "object") {  // an ID reference or
+  if (typeof(member) !== "object") {  // an ID reference or
     countryIdRef = member;
   } else {                       // an object reference
     countryIdRef = member.name;
   }
-  constraintViolation = InternationalOrganizations.checkMember( countryIdRef );
+  constraintViolation = InternationalOrganizations.checkMember(countryIdRef);
   if (countryIdRef &&
       constraintViolation instanceof NoConstraintViolation) {
     // add the new author reference
@@ -171,7 +190,10 @@ InternationalOrganizations.prototype.addMember = function (member) {
   }
 };
 
-/***************Remove Member**************************************/
+/**
+ * Remove Member
+ **/
+
 InternationalOrganizations.prototype.removeMember = function (member) {
   var constraintViolation = null;
   var countryIdRef = "";
@@ -182,7 +204,7 @@ InternationalOrganizations.prototype.removeMember = function (member) {
   else {
     countryIdRef = member.name;
   }
-  constraintViolation = InternationalOrganizations.checkMember( countryIdRef );
+  constraintViolation = InternationalOrganizations.checkMember(countryIdRef);
   if (constraintViolation instanceof NoConstraintViolation) {
     // delete the author reference
     delete this.members[countryIdRef];
@@ -192,7 +214,7 @@ InternationalOrganizations.prototype.removeMember = function (member) {
 };
 
 InternationalOrganizations.convertRow2Obj = function (OrganizationRow) {
-  return new InternationalOrganizations( OrganizationRow );
+  return new InternationalOrganizations(OrganizationRow);
 };
 
 InternationalOrganizations.destroy = function (acronym) {
@@ -200,36 +222,32 @@ InternationalOrganizations.destroy = function (acronym) {
   if (InternationalOrganizations.instances[acronym]) {
     
     delete InternationalOrganizations.instances[acronym];
-    console.log( "InternationalOrganizations " + acronym + " deleted" );
+    console.log("InternationalOrganizations " + acronym + " deleted");
   } else {
-    console.log( "There is no InternationalOrganizations with the name " +
-        acronym + " in the database!" );
+    console.log("There is no InternationalOrganizations with the name " +
+        acronym + " in the database!");
   }
 };
 
 InternationalOrganizations.retrieveAll = function () {
-  
   var key = "", keys = [], organizationString = "", organizations = {}, i = 0;
-  
   try {
-    if (localStorage.getItem( "internationalOrganizations" )) {
-     
-      organizationString = localStorage.getItem( "internationalOrganizations" );
+    if (localStorage.getItem("internationalOrganizations")) {
+      organizationString = localStorage.getItem("internationalOrganizations");
     }
-   
   }
   catch (e) {
-    alert( "Error when reading from Local Storage\n" + e );
+    alert("Error when reading from Local Storage\n" + e);
   }
   if (organizationString) {
-    organizations = JSON.parse( organizationString );
-    keys = Object.keys( organizations );
-    console.log( keys.length + " organizations are loaded" );
+    organizations = JSON.parse(organizationString);
+    keys = Object.keys(organizations);
+    console.log(keys.length + " organizations are loaded");
     
     for (i = 0; i < keys.length; i += 1) {
       key = keys[i];
       InternationalOrganizations.instances[key] =
-          InternationalOrganizations.convertRow2Obj( organizations[key] );
+          InternationalOrganizations.convertRow2Obj(organizations[key]);
     }
   }
 };
@@ -238,17 +256,16 @@ InternationalOrganizations.retrieveAll = function () {
 InternationalOrganizations.saveAll = function () {
   var organizationString = "", error = false;
   var countOfOrganizations =
-      Object.keys( InternationalOrganizations.instances ).length;
+      Object.keys(InternationalOrganizations.instances).length;
   try {
-    organizationString = JSON.stringify( InternationalOrganizations.instances );
-    localStorage.setItem( "internationalOrganizations", organizationString );
+    organizationString = JSON.stringify(InternationalOrganizations.instances);
+    localStorage.setItem("internationalOrganizations", organizationString);
   } catch (e) {
-    alert( "Error" );
+    alert("Error when saving to local storage!");
     error = true;
   }
-  
   if (!error) {
-    console.log( countOfOrganizations + " organizations saved" );
+    console.log(countOfOrganizations + " organizations saved");
   }
 };
 
@@ -256,16 +273,16 @@ InternationalOrganizations.saveAll = function () {
 InternationalOrganizations.add = function (slots) {
   var organization = null;
   try {
-    organization = new InternationalOrganizations( slots );
+    organization = new InternationalOrganizations(slots);
   } catch (e) {
-    console.log( e.constructor.name + ": " + e.message );
+    console.log(e.constructor.name + ": " + e.message);
     organization = null;
   }
-  var validationResult = InternationalOrganizations.checkName( slots.name );
+  var validationResult = InternationalOrganizations.checkName(slots.name);
   if (validationResult instanceof NoConstraintViolation) {
     // add book to the Book.instances collection
     InternationalOrganizations.instances[slots.acronym] = organization;
-    console.log( "Organization " + slots.name + " created!" );
+    console.log("Organization " + slots.name + " created!");
   }
 };
 
@@ -290,25 +307,24 @@ InternationalOrganizations.update = function (slots){
       organization.setMember(slots.members);
       updatedProperties.push("members");
     }
-    
     if ("membersIdRefToAdd" in slots) {
       updatedProperties.push("members(added)");
       var i;
-      for (i=0; i < slots.membersIdRefToAdd.length; i++) {
-        organization.addMember( slots.membersIdRefToAdd[i]);
+      for (i=0; i < slots.membersIdRefToAdd.length; i+=1) {
+        organization.addMember(slots.membersIdRefToAdd[i]);
       }
     }
     if ("membersIdRefToRemove" in slots) {
       updatedProperties.push("members(removed)");
       var i;
-      for (i=0; i < slots.membersIdRefToRemove.length; i++) {
-        organization.removeMember( slots.membersIdRefToRemove[i]);
+      for (i=0; i < slots.membersIdRefToRemove.length; i+=1) {
+        organization.removeMember(slots.membersIdRefToRemove[i]);
       }
       
     }
     console.log("Organization " + slots.acronym + " modified!");
   }catch(e){
-    console.log( e.constructor.name +": "+ e.message);
+    console.log(e.constructor.name +": "+ e.message);
     noConstraintViolated = false;
     // restore object to its state before updating
     InternationalOrganizations.instances[slots.acronym] = objectBeforeUpdate;
@@ -319,7 +335,8 @@ InternationalOrganizations.update = function (slots){
       console.log("Properties " + updatedProperties.toString() +
           " modified for organization " + slots.acronym);
     } else {
-      console.log("No property value changed for organization " + slots.acronym + " !");
+      console.log(
+          "No property value changed for organization " + slots.acronym + " !");
     }
   }
 };
